98c68af6602dff47f9d0dc76a86a804e40625e69
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-24 09:27:58 -0600

    Add barrel export for utility functions

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+// Barrel export for all utilities
+export {
+  groupBy,
+  generateInitials,
+  getTodayDate,
+  generateTempId
+} from './helpers';
+
+export {
+  parseLinks,
+  getBusinessDaysDiff,
+  isEngagementStale,
+  getDaysSinceActivity
+} from './engagement';
