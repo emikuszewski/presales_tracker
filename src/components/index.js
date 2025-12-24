759a1dfa9b8323e85df85e9f1af809aafaeda83e
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-24 10:32:54 -0600

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+// Top-level barrel export for all components
+
+// UI components
+export { 
+  Modal, 
+  OwnersDisplay, 
+  StaleBadge, 
+  NotificationBadge 
+} from './ui';
+
+// Modal components
+export { 
+  LinkModal, 
+  DeleteEngagementModal, 
+  ActivityModal, 
+  PhaseEditModal, 
+  IntegrationsModal, 
+  EditDetailsModal, 
+  OwnersModal, 
+  HistoryModal 
+} from './modals';
+
+// Layout components
+export { AvatarMenu } from './layout';
