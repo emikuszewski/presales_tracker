19f4055db66d7e87016e6b856b004414c4edf7dd
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-24 14:05:56 -0600

    Create index.js

---
+++ src/views/index.js
@@ -0,0 +1,6 @@
+// Barrel export for all views
+export { default as AdminView } from './AdminView';
+export { default as EngagementsAdminView } from './EngagementsAdminView';
+export { default as ListView } from './ListView';
+export { default as DetailView } from './DetailView';
+export { default as NewEngagementView } from './NewEngagementView';
