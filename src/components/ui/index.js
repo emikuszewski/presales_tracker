f46aa95c7cc84f513c1603560721936aed376def
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-24 15:04:26 -0600

    Add SlackIcon export to index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,3 +3,4 @@ export { default as Modal } from './Modal';
 export { default as OwnersDisplay } from './OwnersDisplay';
 export { default as StaleBadge } from './StaleBadge';
 export { default as NotificationBadge } from './NotificationBadge';
+export { default as SlackIcon } from './SlackIcon';
