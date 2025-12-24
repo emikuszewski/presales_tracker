744339ed68b09672583113344d43d5ac3338f5cd
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-24 10:26:44 -0600

    Add barrel export for UI components

---
+++ src/components/ui/index.js
@@ -0,0 +1,5 @@
+// Barrel export for UI components
+export { default as Modal } from './Modal';
+export { default as OwnersDisplay } from './OwnersDisplay';
+export { default as StaleBadge } from './StaleBadge';
+export { default as NotificationBadge } from './NotificationBadge';
