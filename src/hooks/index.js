cfb849688aa18d987f13b838f0883da0de1a7a67
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-25 10:43:19 -0600

    Add new hooks for authentication and team operations

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,7 @@
 // Hooks barrel export
 export { default as usePresalesData } from './usePresalesData';
 export { default as useEngagementDetail } from './useEngagementDetail';
+export { default as useAuth } from './useAuth';
+export { default as useTeamOperations } from './useTeamOperations';
+export { default as useEngagementList } from './useEngagementList';
+export { default as useEngagementCrud } from './useEngagementCrud';
