c89491591bb15a244f62d444f54069a0700a7b1f
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-25 10:02:28 -0600

    Update hooks barrel export comments and structure

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,3 @@
-// Barrel export for all custom hooks
+// Hooks barrel export
 export { default as usePresalesData } from './usePresalesData';
-export { default as useAuth } from './useAuth';
-export { default as useTeamOperations } from './useTeamOperations';
-export { default as useEngagementList } from './useEngagementList';
 export { default as useEngagementDetail } from './useEngagementDetail';
