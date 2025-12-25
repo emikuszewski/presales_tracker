765f97f8a21d48a083565ec0cd1753f82da9cde7
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-25 11:41:13 -0600

    Remove useEngagementCrud export from index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,4 +4,3 @@ export { default as useEngagementDetail } from './useEngagementDetail';
 export { default as useAuth } from './useAuth';
 export { default as useTeamOperations } from './useTeamOperations';
 export { default as useEngagementList } from './useEngagementList';
-export { default as useEngagementCrud } from './useEngagementCrud';
