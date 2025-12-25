bfbfbc580508139249ca4cf70adc8a862b2bfc24
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-25 10:00:51 -0600

    Create engagement index.js for component exports

---
+++ src/components/engagement/index.js
@@ -0,0 +1,9 @@
+// Engagement components barrel export
+export { default as DetailView } from './DetailView';
+export { default as DetailHeader } from './DetailHeader';
+export { default as TabSidebar } from './TabSidebar';
+export { default as TabBottomBar } from './TabBottomBar';
+export { default as ProgressTab } from './ProgressTab';
+export { default as ActivityTab } from './ActivityTab';
+export { default as HistoryTab } from './HistoryTab';
+export { default as NotesTab } from './NotesTab';
