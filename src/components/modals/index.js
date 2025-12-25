72034df9325fdd5379e271378a29cc3a1056f812
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-25 15:39:28 -0600

    Remove unused modal exports and reorder exports

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,9 +1,5 @@
 // Barrel export for modal components
-export { default as LinkModal } from './LinkModal';
 export { default as DeleteEngagementModal } from './DeleteEngagementModal';
-export { default as ActivityModal } from './ActivityModal';
-export { default as PhaseEditModal } from './PhaseEditModal';
 export { default as IntegrationsModal } from './IntegrationsModal';
 export { default as EditDetailsModal } from './EditDetailsModal';
 export { default as OwnersModal } from './OwnersModal';
-export { default as HistoryModal } from './HistoryModal';
