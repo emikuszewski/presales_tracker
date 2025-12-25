b15eb6fb5ccc10bb0c6e568bc3c019909f52c1d8
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-24 18:20:01 -0600

    Update index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -5,4 +5,5 @@ export { default as StaleBadge } from './StaleBadge';
 export { default as NotificationBadge } from './NotificationBadge';
 export { default as SlackIcon } from './SlackIcon';
 export { default as DriveIcon } from './DriveIcon';
+export { default as SlidesIcon } from './SlidesIcon';
 export { default as LinkifyText } from './LinkifyText';
