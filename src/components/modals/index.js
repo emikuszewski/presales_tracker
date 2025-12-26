95bcf19524e8e629349c9732e351ee0a06ef7be6
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-26 13:13:01 -0600

    Add CompetitionModal export to index.js

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -5,3 +5,4 @@ export { default as DeleteEngagementModal } from './DeleteEngagementModal';
 export { default as EditDetailsModal } from './EditDetailsModal';
 export { default as IntegrationsModal } from './IntegrationsModal';
 export { default as OwnersModal } from './OwnersModal';
+export { default as CompetitionModal } from './CompetitionModal';
