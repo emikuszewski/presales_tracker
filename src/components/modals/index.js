6b48a57af41dd2b2b2582c14d40f1533a35f9506
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-26 11:30:50 -0600

    Refactor modal components barrel export
    Updated the modal components barrel export to include new modals and improved comments.

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,5 +1,10 @@
-// Barrel export for modal components
-export { default as DeleteEngagementModal } from './DeleteEngagementModal';
-export { default as IntegrationsModal } from './IntegrationsModal';
+// Modals barrel export
+// Each modal imports its own dependencies internally
+
 export { default as EditDetailsModal } from './EditDetailsModal';
-export { default as OwnersModal } from './OwnersModal';
+export { default as EditLinksModal } from './EditLinksModal';
+export { default as EditOwnersModal } from './EditOwnersModal';
+export { default as AddActivityModal } from './AddActivityModal';
+export { default as EditActivityModal } from './EditActivityModal';
+export { default as ArchiveModal } from './ArchiveModal';
+export { default as DeleteEngagementModal } from './DeleteEngagementModal';
