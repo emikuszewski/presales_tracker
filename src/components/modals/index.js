cf881af7ce11611da1a5fcdfca35ecc6f3e0901e
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-26 11:46:47 -0600

    Refactor modal exports in index.js

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,10 +1,7 @@
 // Modals barrel export
 // Each modal imports its own dependencies internally
 
-export { default as EditDetailsModal } from './EditDetailsModal';
-export { default as EditLinksModal } from './EditLinksModal';
-export { default as EditOwnersModal } from './EditOwnersModal';
-export { default as AddActivityModal } from './AddActivityModal';
-export { default as EditActivityModal } from './EditActivityModal';
-export { default as ArchiveModal } from './ArchiveModal';
 export { default as DeleteEngagementModal } from './DeleteEngagementModal';
+export { default as EditDetailsModal } from './EditDetailsModal';
+export { default as IntegrationsModal } from './IntegrationsModal';
+export { default as OwnersModal } from './OwnersModal';
