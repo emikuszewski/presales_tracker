7e9c1028d4c1d841f11f4fee8717fb94042da9f7
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-12-27 09:38:54 -0600

    Add ConflictModal export to index.js

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -6,3 +6,4 @@ export { default as EditDetailsModal } from './EditDetailsModal';
 export { default as IntegrationsModal } from './IntegrationsModal';
 export { default as OwnersModal } from './OwnersModal';
 export { default as CompetitionModal } from './CompetitionModal';
+export { default as ConflictModal } from './ConflictModal';
