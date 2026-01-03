b86dbc613d524eab6b595525eb6fbab690712406
Ed Mikuszewski <emikuszewski@pivotal.io>
2026-01-02 22:20:22 -0600

    Export AI components: AssistantPanel and AssistantButton
    Added exports for AssistantPanel and AssistantButton components.

--- src/components/ai/index.js
+++ src/components/ai/index.js
@@ -1,1 +1,9 @@
-export { default as CommandPalette } from './CommandPalette';
+/**
+ * AI Components
+ * 
+ * AssistantPanel - Right sidebar panel for AI chat
+ * AssistantButton - Floating action button to open panel
+ */
+
+export { default as AssistantPanel } from './AssistantPanel';
+export { default as AssistantButton } from './AssistantButton';
