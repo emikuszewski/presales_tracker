0119a0db326d5b0da026bf6cbd4b2ed11f9a2e73
Ed Mikuszewski <emikuszewski@pivotal.io>
2026-01-02 18:42:40 -0600

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Amplify } from 'aws-amplify'
 import App from './App.jsx'
 import './index.css'
@@ -19,7 +20,9 @@ async function configureAmplify() {
 configureAmplify().then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>,
   )
 })
