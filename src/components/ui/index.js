f7b6b81e8ea6bf75388d0342f5fc29c0b91efa20
Ed Mikuszewski <emikuszewski@pivotal.io>
2026-01-04 09:53:17 -0600

    Re-export icons for backward compatibility and new icons

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,13 +3,33 @@ export { default as Modal } from './Modal';
 export { default as OwnersDisplay } from './OwnersDisplay';
 export { default as StaleBadge } from './StaleBadge';
 export { default as NotificationBadge } from './NotificationBadge';
-export { default as SlackIcon } from './SlackIcon';
-export { default as DriveIcon } from './DriveIcon';
-export { default as DocsIcon } from './DocsIcon';
-export { default as SlidesIcon } from './SlidesIcon';
-export { default as SheetsIcon } from './SheetsIcon';
 export { default as LinkifyText } from './LinkifyText';
 export { default as IntegrationLinksIndicator } from './IntegrationLinksIndicator';
 export { default as CompetitorLogo } from './CompetitorLogo';
 export { default as CompetitorChips } from './CompetitorChips';
 export { default as EngagementStatusIcon } from './EngagementStatusIcon';
+
+// Icons - re-export from icons folder for backward compatibility
+export { 
+  SlackIcon, 
+  DriveIcon, 
+  DocsIcon, 
+  SlidesIcon, 
+  SheetsIcon,
+  // New extracted icons
+  ArchiveIcon,
+  CheckIcon,
+  ChartIcon,
+  ChatIcon,
+  ClockIcon,
+  DocumentIcon,
+  EllipsisIcon,
+  GearIcon,
+  GlobeIcon,
+  PencilIcon,
+  RefreshIcon,
+  RestoreIcon,
+  UsersIcon,
+  // TabIcons map for TabSidebar/TabBottomBar
+  TabIcons
+} from './icons';
