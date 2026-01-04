3fa945efb883dbf0002e7a22074b4a05a9163805
Ed Mikuszewski <emikuszewski@pivotal.io>
2026-01-04 11:10:01 -0600

    Delete src/components/ai directory

--- src/components/ai/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * AI Components
- * 
- * AssistantPanel - Right sidebar panel for AI chat
- * AssistantButton - Floating action button to open panel
- */
-
-export { default as AssistantPanel } from './AssistantPanel';
-export { default as AssistantButton } from './AssistantButton';
